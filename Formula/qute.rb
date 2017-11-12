09b3a88f7b29e8e6b679bfb00b1db814bd2200b1
maelvalais <mael.valais@gmail.com>
2017-11-12 23:08:25 +0000

    qute: update 0.0.1_1 bottle for Linuxbrew.

--- Formula/qute.rb
+++ Formula/qute.rb
@@ -7,6 +7,12 @@ class Qute < Formula
   revision 1
 
   bottle do
+    root_url "https://dl.bintray.com/touist/bottles-touist"
+    prefix "/usr/local"
+    cellar :any
+    sha256 "8e575716d02136ec6cf3b27e1659427134838b272957327f4d1b84725c7d3b5e" => :sierra
+    sha256 "e75a507e7cd5a90f4775a83bd5873cf0cc0b641d80234ff078bb6d3c57c4c41a" => :el_capitan
+    sha256 "de5b48c9619ebfe7f95cb18c0fe97206592f0b18bf523c3189ff7cdb790c199e" => :x86_64_linux
   end
 
   depends_on "boost"
