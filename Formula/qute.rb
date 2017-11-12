c3c819e92fd2404ecf630f0c47eae4c8ada31ba5
Maël Valais <mael.valais@gmail.com>
2017-11-12 21:18:13 +0100

    added formula for qute (#5)
    WARNING:
    * GitHub fork (not canonical repository)
    * GitHub repository too new (<30 days old)
    
    Also:
    * travis: s3 upload now systematic (after_success -> after_script)

---
+++ Formula/qute.rb
@@ -0,0 +1,29 @@
+class Qute < Formula
+  desc "A dependency learning QBF solver"
+  homepage "https://github.com/maelvalais/qute"
+  url "https://github.com/maelvalais/qute/archive/v0.0.1.tar.gz"
+  sha256 "488825160ac586df7c0c642fff673731ba82647defa12941acde93bcf503a7d8"
+  head "https://github.com/maelvalais/qute.git"
+
+  bottle do
+  end
+
+  depends_on "boost"
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    bin.install "qute"
+  end
+
+  test do
+    (testpath/"test.dimacs").write <<-EOS.undent
+      p cnf 2 1
+      e 2 0
+      a 1 0
+      2 1 0
+    EOS
+    assert_equal "SAT", shell_output("#{bin}/qute #{testpath}/test.dimacs", result = 10).strip
+  end
+end
