1a2616a44aa90c8610378d20a60e8203b2937fac
Maël Valais <mael.valais@gmail.com>
2017-11-13 00:17:39 +0100

    picosat: revision 2: forgot to install the .h

--- Formula/picosat.rb
+++ Formula/picosat.rb
@@ -3,7 +3,7 @@ class Picosat < Formula
   homepage "http://fmv.jku.at/picosat/"
   url "http://fmv.jku.at/picosat/picosat-965.tar.gz"
   sha256 "15169b4f28ba8f628f353f6f75a100845cdef4a2244f101a02b6e5a26e46a754"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://dl.bintray.com/touist/bottles-touist"
@@ -17,8 +17,9 @@ class Picosat < Formula
   def install
     system "./configure.sh"
     system "make"
-    bin.install "picosat","picomcs","picogcnf"
+    bin.install "picosat", "picomcs", "picogcnf", "picomus"
     lib.install "libpicosat.a"
+    lib.install "picosat.h" # because quantor expects everything in one dir
   end
 
   test do
@@ -27,5 +28,13 @@ class Picosat < Formula
       2 1 0
     EOS
     assert_equal "s SATISFIABLE\nv 1 2 0", shell_output("#{bin}/picosat #{testpath}/test.dimacs", result = 10).strip
+  end
+
+  def caveats
+    <<~EOS
+      Because Quantor (and apparently also other solvers) expects both
+      the header and the library to be in one folder, `picosat.h` is
+      installed along with `libpicosat.a` in the lib/ folder.
+    EOS
   end
 end
