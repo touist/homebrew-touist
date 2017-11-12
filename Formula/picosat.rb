e782642e0288974bc7549d217a006ca93c2c6758
maelvalais <mael.valais@gmail.com>
2017-11-12 18:53:44 +0000

    picosat: update 965_1 bottle for Linuxbrew.

--- Formula/picosat.rb
+++ Formula/picosat.rb
@@ -6,6 +6,12 @@ class Picosat < Formula
   revision 1
 
   bottle do
+    root_url "https://dl.bintray.com/touist/bottles-touist"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "4b22ca618eb628806d8715e9598ba3ab6f759d8918c85d9f59e35f35726e1aa3" => :sierra
+    sha256 "1bec1c3c81c41a78c5f06568959b02a0f6e97c33ae05925b18db947b0eaffed9" => :el_capitan
+    sha256 "eb7887245b37d42e8dcb67fd9dbd143c6648ec2f4efac4764088a35c805093ec" => :x86_64_linux
   end
 
   def install
