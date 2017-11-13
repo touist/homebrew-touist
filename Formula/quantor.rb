29a477dc2395fdc3243fa635e59539fe93c40849
maelvalais <mael.valais@gmail.com>
2017-11-13 02:30:06 +0000

    quantor: update 3.2 bottle for Linuxbrew.
    [ci skip]

--- Formula/quantor.rb
+++ Formula/quantor.rb
@@ -5,6 +5,12 @@ class Quantor < Formula
   sha256 "7a82ebfd1c8ecc250325f311e725f6263bf69b412edcc2b600db2a25937d1189"
 
   bottle do
+    root_url "https://dl.bintray.com/touist/bottles-touist"
+    prefix "/usr/local"
+    cellar :any_skip_relocation
+    sha256 "1500ee8e0bd88e02e74d74c57028a98ef6e9f3555a0ee27eba98d7a024fed347" => :sierra
+    sha256 "230115af867ab98a44e434e98e3fadadcb9d88e7bb4458176381f6423b654310" => :el_capitan
+    sha256 "bec41da1aa3154758a66bfd09e90bc5941a4fa1bd141038aff2dbf44fed733c5" => :x86_64_linux
   end
 
   depends_on "picosat" => :build
