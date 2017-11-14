01bca24d4b42405d29f0cc676635caffe1b02564
Maël Valais <mael.valais@gmail.com>
2017-11-14 11:46:36 +0100

    lingeling: force rebuild bottle

--- Formula/lingeling.rb
+++ Formula/lingeling.rb
@@ -1,5 +1,5 @@
 class Lingeling < Formula
-  desc ""
+  desc "A SAT solver"
   homepage "http://fmv.jku.at/lingeling/"
   url "http://fmv.jku.at/lingeling/lingeling-bal-2293bef-151109.tar.gz"
   sha256 "211be9debb67caef07829601b2ea059dbe120470bf33fba6ef79d248baf940a8"
