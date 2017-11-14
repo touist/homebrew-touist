7509a4919fe0b8d2bd86b390cf987b36d9759780
maelvalais <mael.valais@gmail.com>
2017-11-14 11:10:34 +0000

    lingeling: add 151109 bottle for Linuxbrew.
    [ci skip]

--- Formula/lingeling.rb
+++ Formula/lingeling.rb
@@ -4,6 +4,13 @@ class Lingeling < Formula
   url "http://fmv.jku.at/lingeling/lingeling-bal-2293bef-151109.tar.gz"
   sha256 "211be9debb67caef07829601b2ea059dbe120470bf33fba6ef79d248baf940a8"
 
+  bottle do
+    root_url "https://dl.bintray.com/touist/bottles-touist"
+    sha256 "141f132d5ed58f930ada22fbe81d63e4115b9f78b8fb2ca3fa266e30e5fdf0a3" => :sierra
+    sha256 "be1e33155185e36cbfbc48a743efd14d65edbfc174d2e31e4386414ea2153233" => :el_capitan
+    sha256 "b24481b3ac4a7b02114ea5d627aff7cd6e2bb1551e4b4f073d3aa1d580f787c8" => :x86_64_linux
+  end
+
   def install
     system "./configure.sh"
     system "make"
