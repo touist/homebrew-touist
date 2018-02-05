195a21479a55fee56371e063b69bfac9d81ef1f0
Maël Valais <mael.valais@gmail.com>
2018-02-05 19:22:08 +0100

    touist: force rebuild because the touist 3.5.1 was not built

--- touist.rb
+++ touist.rb
@@ -7,13 +7,6 @@ class Touist < Formula
   # We use the git history for `git describe --tags`, so no shallow clone
 
   bottle do
-    root_url "https://dl.bintray.com/touist/bottles-touist"
-    cellar :any_skip_relocation
-    rebuild 7
-    sha256 "415d608143c0c5839b4d6f2319a4cb731ffd5d6bf03fc3f72d14db98680817de" => :high_sierra
-    sha256 "415d608143c0c5839b4d6f2319a4cb731ffd5d6bf03fc3f72d14db98680817de" => :sierra
-    sha256 "77fdf88220486fa59b664a368d2b1f0c81788293588fc2c42a81588bded10af5" => :el_capitan
-    sha256 "4d4a6e7bef7f6732f5b4fe644ddb765b068b56c37fe606e58d00cd23c05bb03a" => :x86_64_linux
   end
 
   option "without-gmp", "Build without gmp which disables the yices2 support"
